2f4b6d9629814fdee05d95f15cc68aeabe875d33
rex <rex@soundlifetv.com>
2015-10-28 17:35:51 +0800

    fix move and add aliyun oss support

--- filebrowser_safe/storage.py
+++ filebrowser_safe/storage.py
@@ -69,46 +69,60 @@ class FileSystemStorageMixin(StorageMixin):
 class S3BotoStorageMixin(StorageMixin):
 
     def isfile(self, name):
-        return self.exists(name) and self.entries[self._normalize_name(self._clean_name(name))].size > 0
+        return (os.path.basename(name) != '.folder') and self._exists(name)
 
     def isdir(self, name):
-        # That's some inefficient implementation...
-        # If there are some files having 'name' as their prefix, then
-        # the name is considered to be a directory
         if not name:  # Empty name is a directory
             return True
 
         if self.isfile(name):
             return False
 
-        name = self._normalize_name(self._clean_name(name))
-        dirlist = self.bucket.list(self._encode_name(name))
-
-        # Check whether the iterator is empty
-        for item in dirlist:
-            return True
-        return False
+        return self._exists(os.path.join(name, '.folder'))
 
     def move(self, old_file_name, new_file_name, allow_overwrite=False):
-
         if self.exists(new_file_name):
-            if allow_overwrite:
-                self.delete(new_file_name)
-            else:
+            if not allow_overwrite:
                 raise "The destination file '%s' exists and allow_overwrite is False" % new_file_name
 
-        old_key_name = self._encode_name(self._normalize_name(self._clean_name(old_file_name)))
-        new_key_name = self._encode_name(self._normalize_name(self._clean_name(new_file_name)))
+            if self.isfile(new_file_name):
+                self.delete(new_file_name)
+            elif self.isdir(new_file_name):
+                self.rmtree(new_file_name)
 
-        k = self.bucket.copy_key(new_key_name, self.bucket.name, old_key_name)
+        if self.isfile(old_file_name):
+            old_key_name = self._encode_name(self._normalize_name(self._clean_name(old_file_name)))
+            new_key_name = self._encode_name(self._normalize_name(self._clean_name(new_file_name)))
 
-        if not k:
-            raise "Couldn't copy '%s' to '%s'" % (old_file_name, new_file_name)
+            k = self.bucket.copy_key(new_key_name, self.bucket.name, old_key_name)
+            if not k:
+                raise "Couldn't copy '%s' to '%s'" % (old_file_name, new_file_name)
+            self.delete(old_file_name)
+        elif self.isdir(old_file_name):
+            oldname = self._normalize_name(self._clean_name(old_file_name))
+            newname = self._normalize_name(self._clean_name(new_file_name))
 
-        self.delete(old_file_name)
+            base_parts = len(oldname.split("/"))
+            for item in self.bucket.list(self._encode_name(oldname)):
+                parts = item.name.split("/")
+                parts = parts[base_parts:]
+                new_item_name = os.path.join(newname, *parts)
+                k = self.bucket.copy_key(self._encode_name(new_item_name), self.bucket.name, self._encode_name(item.name))
+                if not k:
+                    raise "Couldn't copy '%s' to '%s'" % (item.name, new_item_name)
+                self.bucket.delete_key(self._encode_name(item.name))
 
     def makedirs(self, name):
-        self.save(name + "/.folder", ContentFile(""))
+        if not name:
+            return
+
+        dirname, _ = os.path.split(name)
+        self.makedirs(dirname)
+        if self.isfile(name):
+            raise FileExistsError("File exists: '%s'" % name)
+
+        if not self.isdir(name):
+            self.save(os.path.join(name, ".folder"), ContentFile(""))
 
     def rmtree(self, name):
         name = self._normalize_name(self._clean_name(name))
@@ -117,52 +131,5 @@ class S3BotoStorageMixin(StorageMixin):
             item.delete()
 
 
-class GoogleStorageMixin(StorageMixin):
-
-    def isfile(self, name):
-        return self.exists(name)
-
-    def isdir(self, name):
-        # That's some inefficient implementation...
-        # If there are some files having 'name' as their prefix, then
-        # the name is considered to be a directory
-        if not name:  # Empty name is a directory
-            return True
-
-        if self.isfile(name):
-            return False
-
-        name = self._normalize_name(self._clean_name(name))
-        dirlist = self.bucket.list(self._encode_name(name))
-
-        # Check whether the iterator is empty
-        for item in dirlist:
-            return True
-        return False
-
-    def move(self, old_file_name, new_file_name, allow_overwrite=False):
-
-        if self.exists(new_file_name):
-            if allow_overwrite:
-                self.delete(new_file_name)
-            else:
-                raise "The destination file '%s' exists and allow_overwrite is False" % new_file_name
-
-        old_key_name = self._encode_name(self._normalize_name(self._clean_name(old_file_name)))
-        new_key_name = self._encode_name(self._normalize_name(self._clean_name(new_file_name)))
-
-        k = self.bucket.copy_key(new_key_name, self.bucket.name, old_key_name)
-
-        if not k:
-            raise "Couldn't copy '%s' to '%s'" % (old_file_name, new_file_name)
-
-        self.delete(old_file_name)
-
-    def makedirs(self, name):
-        self.save(name + "/.folder", ContentFile(""))
-
-    def rmtree(self, name):
-        name = self._normalize_name(self._clean_name(name))
-        dirlist = self.bucket.list(self._encode_name(name))
-        for item in dirlist:
-            item.delete()
+GoogleStorageMixin = S3BotoStorageMixin
+OSSBotoStorageMixin = S3BotoStorageMixin
